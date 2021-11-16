780ae9ec86e129b40a63efc6f85626e9a8dbfde2
Mark McLeod <markmcleod45@gmail.com>
2021-11-16 15:57:14 +0000

    Add placeholder form at /spaces/new. Add simple feature test and pass it

--- spec/features/list_new_space_spec.rb
+++ spec/features/list_new_space_spec.rb
@@ -3,4 +3,13 @@ feature 'List a new space' do
     visit('/spaces/new')
     expect(page).to have_content 'List a new space'
   end
+
+  scenario 'Displays a form' do
+    visit('/spaces/new')
+    expect(page).to have_button 'List my space'
+    expect(page).to have_field 'name'
+    expect(page).to have_field 'description'
+    expect(page).to have_field 'price_per_night'
+  end
+  
 end
