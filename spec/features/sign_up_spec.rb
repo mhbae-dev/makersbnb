33d37611ad48a4fb38cfa2e97697ccd95e300296
Myoung <mhbae93@gmail.com>
2021-11-16 23:20:29 +0900

    Updated sign_up view to display form. Updated Capybara tests

--- spec/features/sign_up_spec.rb
+++ spec/features/sign_up_spec.rb
@@ -3,4 +3,11 @@ feature 'Sign up' do
     visit('/')
     expect(page).to have_content 'Sign up'
   end
+
+  scenario 'Displays a form' do
+    visit('/')
+    expect(page).to have_button 'Sign up'
+    expect(page).to have_field 'email_address'
+    expect(page).to have_field 'password'
+  end
 end
