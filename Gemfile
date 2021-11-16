734e1eb98ede696ff904e22bd243adb20ecb547d
dean <d.lewis.rave@gmail.com>
2021-11-16 09:18:01 +0000

    project set up

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'capybara'
+gem 'rspec'
+gem 'rubocop'
+gem 'sinatra'
+gem 'sinatra-contrib'
---
+++ app.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+class MakersBnb < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello World'
+  end
+
+  run! if app_file == $PROGRAM_NAME
+end
