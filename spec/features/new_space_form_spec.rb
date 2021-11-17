4ae2f045ea218b6498ccbd2db6b000570261d62c
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-11-17 14:21:49 +0000

    add ability to add dates in the space info and into the database

--- spec/features/new_space_form_spec.rb
+++ spec/features/new_space_form_spec.rb
@@ -10,6 +10,8 @@ feature 'New space form' do
     expect(page).to have_field 'name'
     expect(page).to have_field 'description'
     expect(page).to have_field 'price_per_night'
+    expect(page).to have_field 'available_from'
+    expect(page).to have_field 'available_to'
   end
 
   scenario 'Updates database with space once form is submitted' do
@@ -17,9 +19,12 @@ feature 'New space form' do
     fill_in"name", with: "Tent"
     fill_in"description", with: "8 person tent in field"
     fill_in"price_per_night", with: "20.00"
+    fill_in"available_from", with: "2021-10-28"
+    fill_in"available_to", with: "2021-10-05"
     click_button "List my space"
     expect(page).to have_content "Tent"
     expect(page).to have_content "8 person tent in field, £20.00"
+    expect(page).to have_content "Available from: 2021-10-28, Available to: 2021-10-05"
   end
   
 end
--- spec/features/view_spaces_spec.rb
+++ spec/features/view_spaces_spec.rb
@@ -1,23 +1,17 @@
 require 'pg'
 
+require_relative '../../lib/space.rb'
+
 feature 'view spaces' do
   scenario 'displays spaces' do
 
-    conn = PG.connect(dbname: 'makersbnb_test')
-
     # Add the test data
-    conn.exec("INSERT INTO spaces (name,description,price) VALUES ('hotel','two bed hotelroom', 600.00);")
-    conn.exec("INSERT INTO spaces (name,description,price) VALUES ('penthouse','penthouse w/ hot tub', 1000.00);")
-    conn.exec("INSERT INTO spaces (name,description,price) VALUES ('motel','single bed', 200.00);")
+    Space.add(space_name: 'hotel', space_description: 'two bed hotelroom', space_price: '600.00', available_from: '2021-10-10', available_to: '2021-05-05')
 
     visit '/spaces'
 
     expect(page).to have_content 'hotel' 
     expect(page).to have_content 'bed hotelroom, £600.00'
-    expect(page).to have_content 'penthouse'
-    expect(page).to have_content 'w/ hot tub, £1000.00'
-    expect(page).to have_content 'motel' 
-    expect(page).to have_content 'single bed, £200.00'
-
+    expect(page).to have_content "Available from: 2021-10-10, Available to: 2021-05-05"
   end
 end
