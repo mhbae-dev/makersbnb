9688ee67c31d1c6b4bb53773b95b8656d222c19a
Mark McLeod <markmcleod45@gmail.com>
2021-11-19 10:25:05 +0000

    Encrypted passwords. Tests passing.

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'pg'
+require 'bcrypt'
 
 class User
   attr_reader :id, :email_address
@@ -11,6 +12,7 @@ class User
   end
 
   def self.create(email_address, password)
+    encrypted_password = BCrypt::Password.create(password)
     conn =
       if ENV['ENVIRONMENT'] == 'test'
         PG.connect(dbname: 'makersbnb_test')
@@ -19,7 +21,7 @@ class User
       end
     result =
       conn.exec(
-        "INSERT INTO users (email_address, password) VALUES('#{email_address}', '#{password}') RETURNING id, email_address;",
+        "INSERT INTO users (email_address, password) VALUES('#{email_address}', '#{encrypted_password}') RETURNING id, email_address;",
       )
     User.new(result[0]['id'], result[0]['email_address'])
   end
@@ -34,7 +36,7 @@ class User
       conn.exec("SELECT * FROM users WHERE email_address = '#{email_address}';")
 
     return unless result.any?
-    return unless result[0]['password'] == password
+    return unless BCrypt::Password.new(result[0]['password']) == password
     User.new(result[0]['id'], result[0]['email_address'])
   end
 
